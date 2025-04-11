d6bf47474f8cd2ff91896ac4db55a461ea12f7ca
shanmugavel3 <shanmugavel.muthuraj@gwcdata.ai>
2025-04-11 17:38:25 +0530

    cors origin

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const itemRoutes = require('./routes/itemRoutes');
 
 const app = express();
 const PORT =  4000;
 const MONGO_URI = "mongodb+srv://shanmugavel:Sam123456@items-10-04-2025.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000";
+
+// Enable CORS for all origins (or restrict to specific ones)
+app.use(cors());
+
+// Middleware
 app.use(express.json());
 app.use('/api', itemRoutes);
 
