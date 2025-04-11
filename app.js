f78f203b34370e638405bd4d257ad7030ae34cd7
shanmugavel3 <shanmugavel.muthuraj@gwcdata.ai>
2025-04-11 15:45:16 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const itemRoutes = require('./routes/itemRoutes');
+
+const app = express();
+const PORT =  4000;
+const MONGO_URI = "mongodb+srv://shanmugavel:Sam123456@items-10-04-2025.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000";
+app.use(express.json());
+app.use('/api', itemRoutes);
+
+// Connect to Azure Cosmos DB (MongoDB API)
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
---
+++ routes/itemRoutes.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/items');
+
+// CREATE
+router.post('/items', async (req, res) => {
+  try {
+    const item = new Item({ name: req.body.name });
+    await item.save();
+    console.log("save")
+    res.status(201).json(item);
+  } catch (err) {
+    res.status(400).json({ message: "error" });
+  }
+});
+
+// READ ALL
+router.get('/items', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// READ SINGLE
+router.get('/items/:id', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) return res.status(404).json({ message: "Item not found" });
+    res.json(item);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// UPDATE
+router.put('/items/:id', async (req, res) => {
+  try {
+    const updatedItem = await Item.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      { new: true }
+    );
+    res.json(updatedItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE
+router.delete('/items/:id', async (req, res) => {
+  try {
+    await Item.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Item deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
